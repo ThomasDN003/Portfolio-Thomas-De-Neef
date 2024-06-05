db254544038929b0f7a2121f585a73ba46977778
ThomasDN003 <thomas.de.neef@student.ehb.be>
2024-06-05 21:36:57 +0200

    extra input veld en item opslagen
    Begin om item opslaan lokaal en een input veld voor de hoeveelheid op je lijstje

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-    
+document.title = "Boodschappenlijst";
+
+//opgelsagen lijstje terug halen uit local storage
+function ophalenVanLijstje(){
+    const opgeslagenLijstje = localStorage.getItem('boodschappenlijst')
+    if(opgeslagenLijstje){
+        boodschappenlijst = JSON.parse(opgeslagenLijstje);
+        console.log(boodschappenlijst);
+    }
+}
+
+//item toevoegen oon boodschappenlijst
+function itemToevoegen(item, hoeveelheid) {
+    //eerst kijken of het item als bestaat zo niet dan maken we een nieuwe item aan
+    //aanders verhoog ik gewoon de hoeveelheid
+    if (boodschappenlijst[item]) {
+        boodschappenlijst[item] += hoeveelheid;
+    }
+    else {
+        boodschappenlijst[item] = hoeveelheid;
+    }
+    console.log()
+}
+
