f5767e86540e2aa767772fbc7bab7e4259bbbe39
ThomasDN003 <thomas.de.neef@student.ehb.be>
2024-06-06 22:41:58 +0200

    items weergeven

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 document.title = "Boodschappenlijst";
 
 let boodschappenlijst = JSON.parse(localStorage.getItem("boodschapKey")) || [];
-
-
-
-// //validatie van formulier
-// toevoegKnop.addEventListener("click", itemToevoegen);
+const lijstWeergeven = document.getElementById("boodschappenlijstWeergeven");
+console.log(boodschappenlijst);
 
 const form = document.getElementById("itemToevoegen");
 form.addEventListener("submit", function(event) {
@@ -14,14 +11,29 @@ form.addEventListener("submit", function(event) {
     itemToevoegen(ingegevenItem);
 });
 
+
 function itemToevoegen(ingegevenItem) {
     boodschappenlijst.push(ingegevenItem.trim());
     localStorage.setItem("boodschapKey", JSON.stringify(boodschappenlijst));
     document.getElementById("ingegevenItem").value = "";
+    itemWeergeven();
+
+}   
+
+
+
+function itemWeergeven(ingegevenItem) {
+    lijstWeergeven.innerHTML = "";
+    for (let i = 0; i < boodschappenlijst.length; i++) {
+        lijstWeergeven.innerHTML+= `<li class = "lijstweergeven"><p>${boodschappenlijst[i]} <button data-index="${i}>X</button></p></li>`
+        
+    }
 }
 
 
-
+// function deleteItem(boodschappenlijst[itemToevoegen]) {
+    //     deleteItem
+    // }
 
 //opgelsagen lijstje terug halen uit local storage
 // function ophalenVanLijstje(){
